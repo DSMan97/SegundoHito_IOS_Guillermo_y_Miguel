19af6a41f858793c3afdfeec543ca698a600fc80
GUILLERMO CRESPO AGUAYO <guillermo.crespo@utmac0012.local>
2018-04-19 14:27:29 +0200

    Clase City Añadida

---
+++ TrabajoFinal/City.swift
@@ -0,0 +1,15 @@
+//
+//  City.swift
+//  TrabajoFinal
+//
+//  Created by GUILLERMO CRESPO AGUAYO on 19/4/18.
+//  Copyright © 2018 Miguel Angel Martinez Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class City: NSObject {
+    
+    var
+
+}
